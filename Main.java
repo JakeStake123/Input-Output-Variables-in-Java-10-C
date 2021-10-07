da472fe0e530ac0fcf04ccb415b3bdddda53cee0
JakeStake123 <92122228+JakeStake123@users.noreply.github.com>
2021-10-07 17:25:48 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Shalom world!");
+  }
+}
+
+/* comment*/
+// this is another way to comment//
+
+//Java variable types can be boleans(true or false), strings(words/text in "" ), float(decimals), int(integers/whole numbers), and char(single characters.)
+
+// In math you say x=4 which means 4=x. In programming x=4 (4 is assigned into x)//
+
+/* 
+x="patty"
+b="tomato"
+c="cheese"
+d="bread"
+e="lettuce"
+f="ketchup"
+g="mayo"
+
+So the template is: type variable = value; 
+*/
